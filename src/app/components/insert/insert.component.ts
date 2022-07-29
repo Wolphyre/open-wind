99a2f9cca76608660b6481b8822d426641f73149
Paolo Foppiano <103486420+JusTPaul27@users.noreply.github.com>
2022-07-29 12:55:05 +0200

    added functions

--- src/app/components/insert/insert.component.ts
+++ src/app/components/insert/insert.component.ts
@@ -16,25 +16,68 @@ export class InsertComponent implements OnInit {
 
 
   ngOnInit(): void {
-    
+
   }
 
-  
-  displayCord(lat: string, lng:  string){
+
+  displayCord(lat: string, lng: string) {
     this.weatherServ.verifyValue(lat, lng)
     if (lat && lng) {
       this.weatherServ.getMeteoForecastWithLatLng(lat, lng).subscribe({
         next: data => this.forecastData = data,
-        error: err => console.log(err)      
+        error: err => console.log(err)
       })
     }
-    const imgArrow = document.getElementById('arrow');
-    this.renderer.setStyle(
-      imgArrow,
-      'transform',
-      'rotate(90deg)'
-    )
+
+  }
+  ParseInt(value: string) {
+    return parseInt(value)
   }
 
+  modulo(value: number){
+    if (value < 0 ) return value * -1 
+    return value
+      
+    
+  }
+
+  funzioneDiProva(value: number) {
+    console.log('inizio funzione di prova');
+
+    for (value = 0; value < 366; value++) {
+      console.log('value: ' + value);
+      
+
+      const mod = 360 / 12;
+
+      const degMod = value / 360;
+      
+
+      const turn = Math.floor(degMod);
+      
+      let deg = (degMod - turn) * 360;
+
+      let dir = Math.floor(deg  / mod);
+      console.log('dir: ' + dir);
+
+      dir = dir - Math.floor(dir / 11) * 11;
+      console.log('Nuova dir: ' + dir);
+      
+      switch (dir) {
+        case 0:
+          // console.log('nord');
+
+          // return 'nord'
+          break;
+
+        default:
+          // console.log(dir);
+          // return 'altro'
+      }
+    }
+
+    return
+  }
 
 }
+
--- src/app/components/pipes/arrow-direction/arrow-direction.pipe.ts
+++ src/app/components/pipes/arrow-direction/arrow-direction.pipe.ts
@@ -6,8 +6,30 @@ import { min } from 'rxjs';
 })
 export class ArrowDirectionPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return
+  transform(value: number, ...args: unknown[]): unknown {
+    const mod = 360 / 8;
+    const degMod =  value / 360;
+    console.log('value: ' + value  );
+    
+    const turn = Math.floor(degMod);
+    let deg = (degMod - turn) * 360 ;
+    console.log('deg: ' + deg);
+    
+    let dir = Math.floor( (deg - (mod / 2)) / mod );
+    console.log('dir: ' + dir );
+    
+    switch (dir) {
+      case 0: 
+       console.log('nord');
+       
+        return 'nord'
+      
+    
+      default:
+        console.log(dir);
+        return 'altro'
+    }
   }
+ 
 
 }
