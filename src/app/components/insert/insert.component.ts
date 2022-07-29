082276736c3ec146f6369019d2741ffd7f1005b9
Paolo Foppiano <103486420+JusTPaul27@users.noreply.github.com>
2022-07-29 15:08:02 +0200

    added arrows

--- src/app/components/insert/insert.component.ts
+++ src/app/components/insert/insert.component.ts
@@ -30,54 +30,54 @@ export class InsertComponent implements OnInit {
     }
 
   }
-  ParseInt(value: string) {
-    return parseInt(value)
-  }
+  // ParseInt(value: string) {
+  //   return parseInt(value)
+  // }
 
-  modulo(value: number){
-    if (value < 0 ) return value * -1 
-    return value
+  // modulo(value: number){
+  //   if (value < 0 ) return value * -1 
+  //   return value
       
     
-  }
+  // }
 
-  funzioneDiProva(value: number) {
-    console.log('inizio funzione di prova');
+  // funzioneDiProva(value: number) {
+  //   console.log('inizio funzione di prova');
 
-    for (value = 0; value < 366; value++) {
-      console.log('value: ' + value);
+  //   for (value = 0; value < 366; value++) {
+  //     console.log('value: ' + value);
       
 
-      const mod = 360 / 12;
+  //     const mod = 360 / 12;
 
-      const degMod = value / 360;
+  //     const degMod = value / 360;
       
 
-      const turn = Math.floor(degMod);
+  //     const turn = Math.floor(degMod);
       
-      let deg = (degMod - turn) * 360;
+  //     let deg = (degMod - turn) * 360;
 
-      let dir = Math.floor(deg  / mod);
-      console.log('dir: ' + dir);
+  //     let dir = Math.floor(deg  / mod);
+  //     console.log('dir: ' + dir);
 
-      dir = dir - Math.floor(dir / 11) * 11;
-      console.log('Nuova dir: ' + dir);
+  //     dir = dir - Math.floor(dir / 11) * 11;
+  //     console.log('Nuova dir: ' + dir);
       
-      switch (dir) {
-        case 0:
-          // console.log('nord');
+  //     switch (dir) {
+  //       case 0:
+  //         // console.log('nord');
 
-          // return 'nord'
-          break;
+  //         // return 'nord'
+  //         break;
 
-        default:
-          // console.log(dir);
-          // return 'altro'
-      }
-    }
+  //       default:
+  //         // console.log(dir);
+  //         // return 'altro'
+  //     }
+  //   }
 
-    return
-  }
+  //   return
+  // }
 
 }
 
--- src/app/components/pipes/arrow-direction/arrow-direction.pipe.ts
+++ src/app/components/pipes/arrow-direction/arrow-direction.pipe.ts
@@ -7,29 +7,45 @@ import { min } from 'rxjs';
 export class ArrowDirectionPipe implements PipeTransform {
 
   transform(value: number, ...args: unknown[]): unknown {
+
     const mod = 360 / 8;
-    const degMod =  value / 360;
-    console.log('value: ' + value  );
+
+    const degMod = value / 360;
     
+
     const turn = Math.floor(degMod);
-    let deg = (degMod - turn) * 360 ;
-    console.log('deg: ' + deg);
     
-    let dir = Math.floor( (deg - (mod / 2)) / mod );
-    console.log('dir: ' + dir );
+    let deg = (degMod - turn) * 360;
+
+    let dir = Math.floor(deg  / mod);
+    console.log('dir: ' + dir);
+
+    dir = dir - Math.floor(dir / 8) * 8;
+    console.log('Nuova dir: ' + dir);
     
     switch (dir) {
-      case 0: 
-       console.log('nord');
-       
-        return 'nord'
-      
-    
+      case 0:
+        return './assets/arrow/north-east.svg';
+      case 1: 
+        return './assets/arrow/north-east.svg' ;
+      case 2:
+        return './assets/arrow/east.svg';
+      case 3:
+        return './assets/arrow/south-east.svg'  
+      case 4: 
+        return './assets/arrow/south.svg';
+      case 5:
+        return './assets/arrow/south-west.svg';
+      case 6: 
+        return './assets/arrow/west.svg';
+      case 7 :
+        return './assets/arrow/north-west.svg';
       default:
         console.log(dir);
-        return 'altro'
+        return 'errore'
+    }
     }
   }
  
 
-}
+
